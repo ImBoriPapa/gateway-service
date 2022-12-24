fef9bf53da544d3c5c45264d14121fdfd289ea24
cszzang89@gamail.com <cszzang89@gmail.com>
2022-12-24 17:41:13 +0900

    Initial commit

---
+++ src/test/java/com/bmarket/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmarket.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
